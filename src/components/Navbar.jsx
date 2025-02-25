3123f4950422c9a4fc6661a276238bf286ac1b12
Samyak jain <samyakjain137@gmail.com>
2025-02-25 23:06:48 +0530

    setup routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -6,9 +7,15 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold text-pink-600">CakeShop</h1>
         <ul className="flex space-x-6">
-          <li className="text-gray-700 hover:text-pink-500 cursor-pointer">Home</li>
-          <li className="text-gray-700 hover:text-pink-500 cursor-pointer">Cakes</li>
-          <li className="text-gray-700 hover:text-pink-500 cursor-pointer">Cart</li>
+          <li className="text-gray-700 hover:text-pink-500 cursor-pointer">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="text-gray-700 hover:text-pink-500 cursor-pointer">
+            <Link to="/products">Products</Link>
+          </li>
+          <li className="text-gray-700 hover:text-pink-500 cursor-pointer">
+            <Link to="/cart">Cart</Link>
+          </li>
         </ul>
       </div>
     </nav>
